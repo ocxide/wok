f65f14f56bc4ed5646f481f7db12b583a4136890
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2025-07-10 16:50:32 -0500

    feat: assets load

---
+++ src/assets.rs
@@ -0,0 +1,67 @@
+use lump_core::{
+    error::LumpUnknownError,
+    prelude::{Commands, Param, ResMut, Resource},
+};
+
+pub use loaders::*;
+
+pub struct AssetInit<'r, R: Resource> {
+    commands: Commands<'r>,
+    _marker: std::marker::PhantomData<fn(R)>,
+}
+
+impl<'r, R: Resource> Param for AssetInit<'r, R> {
+    type Owned = <Commands<'r> as Param>::Owned;
+    type AsRef<'p> = AssetInit<'p, R>;
+
+    fn init(rw: &mut lump_core::world::SystemLock) {
+        ResMut::<R>::init(rw);
+    }
+
+    fn get(world: &lump_core::prelude::WorldState) -> Self::Owned {
+        <Commands<'r> as Param>::get(world)
+    }
+
+    fn as_ref(owned: &Self::Owned) -> Self::AsRef<'_> {
+        AssetInit {
+            commands: <Commands<'r> as Param>::as_ref(owned),
+            _marker: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<'r, R: Resource> AssetInit<'r, R> {
+    pub async fn with(mut self, loader: impl AssetLoader<R>) -> Result<(), LumpUnknownError> {
+        let resource = loader.load().await?;
+        self.commands.insert_resource(resource);
+
+        Ok(())
+    }
+}
+
+pub trait AssetLoader<T> {
+    fn load(self) -> impl Future<Output = Result<T, LumpUnknownError>>;
+}
+
+pub mod loaders {
+    use std::path::Path;
+
+    use lump_core::error::LumpUnknownError;
+
+    use super::AssetLoader;
+
+    pub struct TomlLoader<P: AsRef<Path> + 'static + Send>(pub P);
+
+    impl<T, P> AssetLoader<T> for TomlLoader<P>
+    where
+        T: serde::de::DeserializeOwned,
+        P: AsRef<Path> + Send,
+    {
+        async fn load(self) -> Result<T, LumpUnknownError> {
+            let buf = tokio::task::spawn_blocking(move || std::fs::read_to_string(self.0.as_ref()))
+                .await??;
+
+            toml::from_str(&buf).map_err(Into::into)
+        }
+    }
+}
