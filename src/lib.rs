599e9b50baf51af775edbaafc78fb44a4dca21ce
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2025-08-28 13:57:07 -0500

    refactor(lib): remove events

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,12 @@ pub mod prelude {
     pub use lump_core::error::LumpUnknownError;
     pub use lump_core::prelude::*;
 
-    pub use crate::events::{Event, Events, OnEvents, EventSender};
     pub use crate::startup::Startup;
 
     pub use crate::plugin::Plugin;
 }
 
 pub mod app;
-mod events;
 pub mod foreign;
 pub mod plugin;
 pub(crate) mod runtime;
