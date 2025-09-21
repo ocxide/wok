2097d944020bdc658c2150e08cbab67455a2dc3a
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2025-09-21 13:31:54 -0500

    refactor(derive): parse required attrs

--- libs/wok_derive/src/lib.rs
+++ libs/wok_derive/src/lib.rs
@@ -4,6 +4,180 @@ use syn::{
     spanned::Spanned,
 };
 
+mod derime {
+    use std::str::FromStr;
+
+    use proc_macro2::Span;
+    use syn::{
+        Expr, Ident, MetaNameValue, Token, parse::Parser, punctuated::Punctuated, spanned::Spanned,
+    };
+
+    use crate::{CompileError, span_compile_error};
+
+    fn key_value(
+        attr: &syn::Attribute,
+        namespace: &str,
+    ) -> Result<Punctuated<MetaNameValue, Token![,]>, CompileError> {
+        let tokens = match &attr.meta {
+            syn::Meta::List(meta_list) => &meta_list.tokens,
+            _ => return Err(span_compile_error!(attr.span() => "Expected #[{}(...)]", namespace)),
+        };
+
+        let keyvalues = Punctuated::<MetaNameValue, Token![,]>::parse_terminated
+            .parse2(tokens.clone())
+            .map_err(|_| span_compile_error!(attr.span() => "Invalid attribute syntax"))?;
+
+        if keyvalues.is_empty() {
+            return Err(
+                span_compile_error!(attr.span() => "Expected at least one key-value pair in #[{}(...)]", namespace),
+            );
+        }
+
+        Ok(keyvalues)
+    }
+
+    pub trait AttrsMatch<Marker> {
+        type Out;
+
+        fn attrs_match(
+            self,
+            span: Span,
+            key_values: impl Iterator<Item = MetaNameValue>,
+        ) -> Result<Self::Out, CompileError>;
+    }
+
+    impl<Marker1, K1: PathMatch + ReprValueRef, V1: ValueParse<Marker1>> AttrsMatch<Marker1>
+        for (K1, V1)
+    {
+        type Out = V1::Out;
+        fn attrs_match(
+            self,
+            span: Span,
+            mut key_values: impl Iterator<Item = MetaNameValue>,
+        ) -> Result<Self::Out, CompileError> {
+            let Some(kv) = key_values.find(|kv| self.0.path_match(&kv.path)) else {
+                return Err(
+                    span_compile_error!(span => "Expected at least one single {} = ...", K1::repr()),
+                );
+            };
+
+            if key_values.next().is_some() {
+                return Err(
+                    span_compile_error!(span => "Expected at most one single {} = {}", K1::repr(), V1::repr()),
+                );
+            };
+
+            self.1.parse(kv.value)
+        }
+    }
+
+    pub struct KeyIdent<'s>(pub &'s str);
+
+    impl ReprValueRef for KeyIdent<'_> {
+        fn repr(&self) -> &str {
+            self.0
+        }
+    }
+
+    trait ReprValueRef {
+        fn repr(&self) -> &str;
+    }
+
+    pub trait PathMatch {
+        fn path_match(&self, path: &syn::Path) -> bool;
+    }
+
+    impl PathMatch for KeyIdent<'_> {
+        fn path_match(&self, path: &syn::Path) -> bool {
+            path.is_ident(self.0)
+        }
+    }
+
+    pub trait ValueParse<Marker> {
+        type Out;
+        fn parse(self, expr: Expr) -> Result<Self::Out, CompileError>;
+    }
+
+    pub struct IdentValue<T>(std::marker::PhantomData<T>);
+
+    impl<T> IdentValue<T> {
+        pub const fn new() -> Self {
+            Self(std::marker::PhantomData)
+        }
+    }
+
+    impl<T> Default for IdentValue<T> {
+        fn default() -> Self {
+            Self::new()
+        }
+    }
+
+    impl<M, I: IdentValueParse<M>> ValueParse<M> for IdentValue<I> {
+        type Out = I;
+
+        fn parse(self, expr: Expr) -> Result<Self::Out, CompileError> {
+            let span = expr.span();
+            let segments = match expr {
+                Expr::Path(syn::ExprPath { path, .. }) => path.segments,
+                _ => return Err(span_compile_error!(expr.span() => "Expected an identifier")),
+            };
+
+            let ident = segments
+                .into_iter()
+                .next()
+                .map(|s| s.ident)
+                .ok_or_else(|| span_compile_error!(span => "Expected an identifier"))?;
+
+            I::parse(ident)
+        }
+    }
+
+    pub trait ReprValue {
+        fn repr() -> &'static str;
+    }
+
+    pub trait IdentValueParse<Marker>: Sized {
+        fn repr() -> &'static str;
+        fn parse(ident: Ident) -> Result<Self, CompileError>;
+    }
+
+    pub struct IdentFromStr;
+    impl<S: FromStr + ReprValue> IdentValueParse<IdentFromStr> for S {
+        fn repr() -> &'static str {
+            S::repr()
+        }
+
+        fn parse(ident: Ident) -> Result<Self, CompileError> {
+            S::from_str(&ident.to_string())
+                .map_err(|e| span_compile_error!(ident.span() => "{}", e))
+        }
+    }
+
+    pub fn parse_attrs<Marker, P: AttrsMatch<Marker>>(
+        namespace: &str,
+        attrs: &[syn::Attribute],
+        parser: P,
+    ) -> Result<P::Out, CompileError> {
+        let span = match attrs {
+            [] => {
+                return Err(
+                    span_compile_error!(Span::call_site() => "Expected at least one #[{}()] attribute", namespace),
+                );
+            }
+            [attr] => attr.span(),
+            [attr1, .., attr2] => attr1.span().join(attr2.span()).unwrap(),
+        };
+
+        let entries: Vec<_> = attrs
+            .iter()
+            .map(|attr| key_value(&attr, namespace))
+            .collect::<Result<_, _>>()?;
+
+        let key_values = entries.into_iter().flatten();
+        parser.attrs_match(span, key_values)
+    }
+}
+
 #[proc_macro_derive(Param, attributes(param))]
 pub fn param_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = syn::parse_macro_input!(input as syn::DeriveInput);
@@ -11,6 +185,51 @@ pub fn param_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         Ok(tokens) => tokens.into(),
         Err(err) => err.into(),
     }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Usage {
+    Core,
+    Lib,
+    Crate,
+}
+
+impl derime::ReprValue for Usage {
+    fn repr() -> &'static str {
+        "core|lib|crate"
+    }
+}
+
+struct StaticErr(&'static str);
+
+impl std::fmt::Display for StaticErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl std::str::FromStr for Usage {
+    type Err = StaticErr;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "core" => Ok(Usage::Core),
+            "lib" => Ok(Usage::Lib),
+            "crate" => Ok(Usage::Crate),
+            _ => Err(StaticErr("expected core|lib|crate")),
+        }
+    }
+}
+
+fn get_usage(attrs: &[syn::Attribute]) -> Result<Usage, CompileError> {
+    derime::parse_attrs(
+        "params",
+        attrs,
+        (
+            derime::KeyIdent("usage"),
+            derime::IdentValue::<Usage>::new(),
+        ),
+    )
 }
 
 fn do_param_derive(ast: syn::DeriveInput) -> Result<proc_macro2::TokenStream, CompileError> {
@@ -332,11 +551,28 @@ impl From<CompileError> for proc_macro2::TokenStream {
     }
 }
 
-macro_rules! span_compile_error(($span: expr => $msg: expr) => {
-    crate::CompileError(quote::quote_spanned! { $span => compile_error!($msg) }.into())
-});
+macro_rules! span_compile_error(
+    ($span: expr => $msg: expr) => {
+        crate::CompileError(quote::quote_spanned! { $span => compile_error!($msg) }.into())
+    };
+
+    ($span: expr => $msg: expr, $( $param: expr ),*) => {
+        crate::CompileError(quote::quote_spanned! { $span => compile_error!($msg, $( $param ),*) }.into())
+    }
+);
 
 pub(crate) use span_compile_error;
+
+#[test]
+fn parses_attrs() {
+    let input: syn::DeriveInput = syn::parse_quote! {
+        #[param(usage = lib)]
+        struct Foo;        
+    };
+
+    let usage = get_usage(input.attrs.as_slice()).unwrap();
+    assert_eq!(usage, Usage::Lib);
+}
 
 #[test]
 fn empty() {
